ce3ee07f1a86530782b55fa8f781d9070c124c26
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-05-24 02:27:19 +0200

    update

--- build-src-Desktop_Qt_5_12_12_MinGW_32_bit-Debug/CMakeFiles/CheckIncludeFiles/HAVE_DLFCN_H.c
+++
@@ -1,9 +0,0 @@
-/* */
-#include <sys/types.h>
-#include <ctype.h>
-#include <direct.h>
-#include <dlfcn.h>
-
-
-int main(void){return 0;}
-
--- build-src-Desktop_Qt_5_12_12_MinGW_32_bit-Debug/dependencies/zlib-1.2.11/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/rapha/Documents/GitHub/OpenHD-ImageWriter/src/dependencies/zlib-1.2.11
-# Build directory: C:/Users/rapha/Documents/GitHub/OpenHD-ImageWriter/build-src-Desktop_Qt_5_12_12_MinGW_32_bit-Debug/dependencies/zlib-1.2.11
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
